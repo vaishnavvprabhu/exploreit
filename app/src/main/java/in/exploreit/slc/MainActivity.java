6fa9ff4c55667cff4912013667670020d6693f00
Jay <jayprimerathod@gmail.com>
2022-03-17 16:16:05 +0530

    Added nav dependencies & fading splash screen.

--- app/src/main/java/in/exploreit/slc/MainActivity.java
+++ app/src/main/java/in/exploreit/slc/MainActivity.java
@@ -1,14 +1,42 @@
 package in.exploreit.slc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.view.animation.AnticipateInterpolator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
+
+import in.exploreit.slc.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        SplashScreen.installSplashScreen(this).setOnExitAnimationListener(
+                splashScreenViewProvider -> {
+                    ObjectAnimator fade = ObjectAnimator.ofFloat(
+                            splashScreenViewProvider.getIconView(),
+                            "alpha",
+                            1.0f,
+                            0.0f
+                    );
+                    fade.setInterpolator(new AnticipateInterpolator());
+                    fade.setDuration(500L);
+                    fade.addListener(new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {splashScreenViewProvider.remove();}
+                        @Override public void onAnimationStart(Animator animation) { }
+                        @Override public void onAnimationCancel(Animator animation) { }
+                        @Override public void onAnimationRepeat(Animator animation) { }
+                    });
+                    fade.start();
+                }
+        );
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.meow.setText(R.string.app_name);
     }
 }
