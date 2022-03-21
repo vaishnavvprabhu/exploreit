207cd5162066ed54a59f9465ccb75d5e338cb8f2
Jay <jayprimerathod@gmail.com>
2022-03-21 12:05:59 +0530

    Setup Basic navigation to test UI designs

---
+++ app/src/main/java/in/exploreit/slc/fragments/OTPFragment.java
@@ -0,0 +1,36 @@
+package in.exploreit.slc.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+
+import in.exploreit.slc.MainActivity;
+import in.exploreit.slc.R;
+
+public class OTPFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_otp, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        view.findViewById(R.id.home_btn).setOnClickListener(
+                textView -> {
+                    MainActivity activity = (MainActivity) getActivity();
+                    NavController navController = activity.getNavController();
+                    if(navController != null) {
+                        navController.navigate(R.id.action_OTPFragment_to_homeFragment);
+                    }
+                }
+        );
+    }
+}
