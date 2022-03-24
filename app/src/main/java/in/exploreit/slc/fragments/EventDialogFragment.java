0d784cc1216830759ebdc5d526bae2f018bcd4b7
vibha <vig2021128@sicsr.ac.in>
2022-03-24 22:55:44 +0530

    GIT Push Log 24-03-22 22:58pm dialog fragment designed dialog animation files created dialog fragment java files created(not tested as navigation is not present)

---
+++ app/src/main/java/in/exploreit/slc/fragments/EventDialogFragment.java
@@ -0,0 +1,39 @@
+package in.exploreit.slc.fragments;
+
+import android.app.Dialog;
+import androidx.fragment.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import in.exploreit.slc.R;
+
+public class EventDialogFragment extends DialogFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(EventDialogFragment.STYLE_NORMAL, R.style.DialogFullScreen);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog d = getDialog();
+        if (d!=null){
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            d.getWindow().setLayout(width, height);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_dialog, container, false);
+
+        requireDialog().getWindow().setWindowAnimations(
+                R.style.DialogAnimation
+        );
+        return root;
+    }
+}
