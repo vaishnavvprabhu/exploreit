186403d554012ac8c42cb614bbb11ac0256187bc
vaishnavvprabhu <67993582+vaishnavvprabhu@users.noreply.github.com>
2022-03-24 11:50:43 +0530

    Changelog: - OTP Functionality now working - Added goToOTP button which will transform to become the sign in button

---
+++ app/src/main/java/in/exploreit/slc/repository/commonAuthRepository.java
@@ -0,0 +1,138 @@
+/*
+package in.exploreit.slc.repository;
+
+import static android.content.ContentValues.TAG;
+
+import android.app.Application;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+import in.exploreit.slc.MainActivity;
+
+public class commonAuthRepository {
+    private Application application;
+    private MutableLiveData<FirebaseUser> firebaseUserMutableLiveData;
+    private MutableLiveData<Boolean> userLoggedMutableLiveData;
+    private FirebaseAuth auth;
+
+    public MutableLiveData<FirebaseUser> getFirebaseUserMutableLiveData() {
+        return firebaseUserMutableLiveData;
+    }
+
+    public MutableLiveData<Boolean> getUserLoggedMutableLiveData() {
+        return userLoggedMutableLiveData;
+    }
+
+    public commonAuthRepository(Application applciation){
+        this.application = application;
+        firebaseUserMutableLiveData = new MutableLiveData<>();
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null){
+            //if user is already logged in
+            firebaseUserMutableLiveData.postValue(auth.getCurrentUser());
+        }
+    }
+
+    public void register(String mnum){
+        PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential credential) {
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+                Log.d(TAG, "onVerificationCompleted:" + credential);
+
+                signInWithPhoneAuthCredential(credential);
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+                Log.w(TAG, "onVerificationFailed", e);
+
+                if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                } else if (e instanceof FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
+                }
+
+                // Show a message and update the UI
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId,
+                                   @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:" + verificationId);
+
+                // Save verification ID and resending token so we can use them later
+                String mVerificationId = verificationId;
+                PhoneAuthProvider.ForceResendingToken mResendToken = token;
+            }
+        };
+
+    }
+
+    public void signInWithPhoneAuthCredential(PhoneAuthCredential credential){
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+
+                            FirebaseUser user = task.getResult().getUser();
+                            // Update UI
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                            }
+                        }
+                    }
+                });
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
