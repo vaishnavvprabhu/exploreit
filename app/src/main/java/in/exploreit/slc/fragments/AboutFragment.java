5ddf70a3468b5fcdd7648f04878b007b095a6a22
vaish <vas2021123@sicsr.ac.in>
2022-03-27 18:22:35 +0530

    -home page rect colour changed to btn_blue -cardview elevated,textviews updated -added about us custom top app bar -themes file:top-bar colour changed to btn_blue

--- app/src/main/java/in/exploreit/slc/fragments/AboutFragment.java
+++ app/src/main/java/in/exploreit/slc/fragments/AboutFragment.java
@@ -1,10 +1,16 @@
 package in.exploreit.slc.fragments;
 
+import static com.firebase.ui.auth.AuthUI.getApplicationContext;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import in.exploreit.slc.R;
@@ -15,6 +21,27 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        //return inflater.inflate(R.layout.fragment_about, container, false);
+        View v = inflater.inflate(R.layout.fragment_about, container, false);
+        Toolbar toolbar = (Toolbar) v.findViewById(R.id.toolbarAboutus);
+
+        toolbar.setNavigationIcon(R.drawable.back_icon);
+        toolbar.setNavigationOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                getActivity().onBackPressed();
+
+            }
+
+                                                  // do something when click navigation
+        });
+        return v;
+
+
+
+
+
     }
+
+
 }
