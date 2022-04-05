c96ebdc3e4c41f5d89c551b7b68f49530629e869
vaish <vas2021123@sicsr.ac.in>
2022-04-05 14:00:44 +0530

    about frag

--- app/src/main/java/in/exploreit/slc/fragments/AboutFragment.java
+++ app/src/main/java/in/exploreit/slc/fragments/AboutFragment.java
@@ -1,47 +1,35 @@
 package in.exploreit.slc.fragments;
 
-import static com.firebase.ui.auth.AuthUI.getApplicationContext;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import in.exploreit.slc.R;
+import in.exploreit.slc.utils.Utils;
 
 public class AboutFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_about, container, false);
-        View v = inflater.inflate(R.layout.fragment_about, container, false);
-        Toolbar toolbar = (Toolbar) v.findViewById(R.id.toolbarAboutus);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbarExploreiT);
 
         toolbar.setNavigationIcon(R.drawable.back_icon);
-        toolbar.setNavigationOnClickListener( new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                getActivity().onBackPressed();
-
-            }
-
-                                                  // do something when click navigation
-        });
-        return v;
-
-
-
-
-
+        toolbar.setNavigationOnClickListener(backButton -> getActivity().onBackPressed());
+        return view;
     }
 
-
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        view.findViewById(R.id.contribute_btn).setOnClickListener(button -> {
+            Utils.openWebpage(this.requireContext(), "https://exploreit.in/");
+        });
+    }
 }
