d1cfd013f4a4369938f4f9c8852ed0f01da1eed7
J A Y <jayprimerathod@gmail.com>
2022-04-07 09:57:56 +0530

    Fixes to venue data.

--- app/src/main/java/in/exploreit/slc/data/models/Event.java
+++ app/src/main/java/in/exploreit/slc/data/models/Event.java
@@ -3,51 +3,19 @@ package in.exploreit.slc.data.models;
 import java.util.Date;
 
 public class Event implements ListItem{
-    private String name;
-    private String img;
-    private String desc;
-    private Date date_time;
-    private Boolean paid;
-    private int price;
-    private int hours;
-    private String target_url;
-    private String venue;
-
-    public String getName() { return name; }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setTarget_url(String target_url) { this.target_url = target_url;}
-
-    public String getImg() { return img; }
-
-    public void setImg(String img) { this.img = img; }
-
-    public String getDesc() { return desc; }
-
-    public void setDesc(String desc) { this.desc = desc; }
-
-    public Date getDate_time() { return date_time; }
-
-    public void setDate_time(Date date_time) { this.date_time = date_time; }
-
-    public Boolean getPaid() { return paid; }
-
-    public void setPaid(Boolean paid) { this.paid = paid; }
+    public String name;
+    public String img;
+    public String desc;
+    public Date date_time;
+    public Boolean paid;
+    public int price;
+    public int hours;
+    public String target_url;
+    public String venue;
 
     public int getPrice() { return price; }
 
-    public void setPrice(int price) { this.price = price; }
-
-    public int getHours() { return hours; }
-
-    public void setHours(int time) { this.hours = time; }
-
     public String getVenue() { return venue; }
-
-    public void setVenue(String venue) { this.venue = venue; }
 
     @Override public String getTitle() { return name; }
 
